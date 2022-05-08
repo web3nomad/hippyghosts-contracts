6702b1e2806d8127b05b40f3a327775471059d14
web3nomad.eth <me@web3nomad.eth>
2022-05-08 12:30:08 +0800

    add deploy script to set address

---
+++ deploy/2.SetAddresses.js
@@ -0,0 +1,42 @@
+module.exports = async ({
+  getNamedAccounts, deployments, getChainId, ethers
+}) => {
+  const chainId = +(await getChainId());
+  const {
+    deployer,
+    whitelistVerificationAddress,
+  } = await getNamedAccounts();
+
+  const [
+    HippyGhosts,
+    HippyGhostsRenderer,
+    HippyGhostsMinter,
+  ] = await Promise.all([
+    deployments.get('HippyGhosts'),
+    deployments.get('HippyGhostsRenderer'),
+    deployments.get('HippyGhostsMinter'),
+  ]);
+
+  const signer = await ethers.getSigner(deployer);
+  const hippyGhosts = new ethers.Contract(HippyGhosts.address, HippyGhosts.abi, signer);
+  const [
+    mintController,
+    renderer,
+  ] = await Promise.all([
+    hippyGhosts.mintController(),
+    hippyGhosts.renderer(),
+  ]);
+
+  if (
+    mintController === '0x0000000000000000000000000000000000000000' &&
+    renderer === '0x0000000000000000000000000000000000000000'
+  ) {
+    const tx = await hippyGhosts.setAddresses(HippyGhostsRenderer.address, HippyGhostsMinter.address);
+    console.log(`setAddresses ${HippyGhostsRenderer.address} ${HippyGhostsMinter.address}`);
+    console.log(`tx: ${tx.hash}`);
+    await tx.wait();
+  }
+
+}
+
+module.exports.tags = ['connect'];
